96dd869cfda6a8412f281d15e95966ee3a7be135
GayanthaSandeep16 <gayantha.20210118@iit.ac.lk>
2025-07-20 20:42:23 +0530

    project init

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,29 +1,22 @@
-import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
-import { useFonts } from 'expo-font';
-import { Stack } from 'expo-router';
-import { StatusBar } from 'expo-status-bar';
-import 'react-native-reanimated';
+import { Slot, Stack } from "expo-router";
+import { ClerkLoaded, ClerkProvider } from "@clerk/clerk-expo";
 
-import { useColorScheme } from '@/hooks/useColorScheme';
+const clerkPublishKey = process.env.EXPO_PUBLIC_CLERK_PUBLISHABLE_KEY;
+
+const InitialLayout = () => {
+  return (
+    <Stack>
+      <Stack.Screen name="index" />
+    </Stack>
+  );
+};
 
 export default function RootLayout() {
-  const colorScheme = useColorScheme();
-  const [loaded] = useFonts({
-    SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf'),
-  });
-
-  if (!loaded) {
-    // Async font loading only occurs in development.
-    return null;
-  }
-
   return (
-    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-      <Stack>
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-        <Stack.Screen name="+not-found" />
-      </Stack>
-      <StatusBar style="auto" />
-    </ThemeProvider>
+    <ClerkProvider publishableKey={clerkPublishKey!}>
+      <ClerkLoaded>
+        <InitialLayout />
+      </ClerkLoaded>
+    </ClerkProvider>
   );
 }
